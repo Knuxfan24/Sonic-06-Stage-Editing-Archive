40b4004b1e05aa930c52a25bef81a11d548030b4
Knuxfan24 <knuxfan@live.co.uk>
2019-06-28 22:16:49 +0100

    Uploaded my GLVL Converter
    It is coded AWFULLY and will likely break a lot, just a heads up.

---
+++ Tools/GLVL Converter/GLVL Converter/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace GLVL_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filepathBox = new System.Windows.Forms.TextBox();
+            this.templatesBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.templatesButton = new System.Windows.Forms.Button();
+            this.filepathButton = new System.Windows.Forms.Button();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // filepathBox
+            // 
+            this.filepathBox.Location = new System.Drawing.Point(104, 27);
+            this.filepathBox.Name = "filepathBox";
+            this.filepathBox.Size = new System.Drawing.Size(335, 20);
+            this.filepathBox.TabIndex = 0;
+            this.filepathBox.TextChanged += new System.EventHandler(this.FilepathBox_TextChanged);
+            // 
+            // templatesBox
+            // 
+            this.templatesBox.Location = new System.Drawing.Point(104, 53);
+            this.templatesBox.Name = "templatesBox";
+            this.templatesBox.Size = new System.Drawing.Size(335, 20);
+            this.templatesBox.TabIndex = 1;
+            this.templatesBox.TextChanged += new System.EventHandler(this.TemplatesBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Generations Set:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "GLVL Templates:";
+            // 
+            // templatesButton
+            // 
+            this.templatesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.templatesButton.Location = new System.Drawing.Point(445, 52);
+            this.templatesButton.Name = "templatesButton";
+            this.templatesButton.Size = new System.Drawing.Size(22, 22);
+            this.templatesButton.TabIndex = 29;
+            this.templatesButton.Text = "...";
+            this.templatesButton.UseVisualStyleBackColor = true;
+            this.templatesButton.Click += new System.EventHandler(this.TemplatesButton_Click);
+            // 
+            // filepathButton
+            // 
+            this.filepathButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.filepathButton.Location = new System.Drawing.Point(445, 26);
+            this.filepathButton.Name = "filepathButton";
+            this.filepathButton.Size = new System.Drawing.Size(22, 22);
+            this.filepathButton.TabIndex = 28;
+            this.filepathButton.Text = "...";
+            this.filepathButton.UseVisualStyleBackColor = true;
+            this.filepathButton.Click += new System.EventHandler(this.FilepathButton_Click);
+            // 
+            // convertButton
+            // 
+            this.convertButton.Enabled = false;
+            this.convertButton.Location = new System.Drawing.Point(392, 80);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 30;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(222)))), ((int)(((byte)(222)))));
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(478, 24);
+            this.menuStrip.TabIndex = 31;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.LoadToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 113);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.templatesButton);
+            this.Controls.Add(this.filepathButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.templatesBox);
+            this.Controls.Add(this.filepathBox);
+            this.Controls.Add(this.menuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox filepathBox;
+        private System.Windows.Forms.TextBox templatesBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button templatesButton;
+        private System.Windows.Forms.Button filepathButton;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+    }
+}
+
---
+++ Tools/GLVL Converter/REAMDE.md
@@ -0,0 +1,5 @@
+An incredibly bodged together tool to convert the Generations SET Data using these templates from this repository to 06, a lot of objects will be setup incorrectly, as most values that needs maths done to them will not be handled correctly without manual code modification.
+
+Objects that have a default target value will also crash the converter, due to GLVL and HedgeLib being rather dumb.
+
+I was holding off on uploading this for a while, but I really feel like I should just get it out there.
