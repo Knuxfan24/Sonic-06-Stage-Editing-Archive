c791f3a345575293c2d7a8a27c88aaffc513fa33
Knuxfan24 <knuxfan@live.co.uk>
2020-05-07 17:46:25 +0100

    Uploaded the Path Scripts and the old Object Peeker

---
+++ Binaries/PATH Maxscripts/README.md
@@ -0,0 +1,5 @@
+# PATH Importer
+Imports splines from Sonic '06 PATH files into 3ds Max.
+
+# PATH Exporter
+Exports splines made in 3ds Max to Sonic '06 PATH files. The splines don't seem to need any actual naming, as they seem to be based on the file itself.
---
+++ Source Code/Sonic '06 Object Peeker/SONIC THE HEDGEHOG (2006) Object Peeker/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SONIC THE HEDGEHOG (2006) Object Peeker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SONIC THE HEDGEHOG (2006) Object Peeker")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ed1f4490-d990-48bf-8d7f-6bc09ebaeded")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
